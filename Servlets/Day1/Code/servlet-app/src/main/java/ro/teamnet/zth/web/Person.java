021c1ac5d70a74604f7f3047394d7522b23e1d38
Alexandru.Negoita <alx.negoita@yahoo.com>
2017-07-18 15:02:56 +0300

    sal2

---
+++ Servlets/Day1/Code/servlet-app/src/main/java/ro/teamnet/zth/web/Person.java
@@ -0,0 +1,19 @@
+package ro.teamnet.zth.web;
+
+/**
+ * Created by Alexandru.Negoita on 7/18/2017.
+ */
+public class Person {
+    public String firtName;
+    public String lastName;
+    public long age;
+    public boolean married;
+
+    @Override
+    public compareTo(long a, long b){
+
+    }
+
+    public static <T> int getScore(T t) {
+    }
+}
